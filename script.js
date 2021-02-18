c99bd29c13d24a6273a90df3bb44a44ff0f45b19
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-18 16:01:10 +0000

    linter errors 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let dog = 0;
 let cat = 0;
-document.getElementById('press').addEventListener('click', myInput)
-function myInput() {
-  dog = Math.floor(Math.random() * 7) + 1;
-  cat = document.getElementById('mine').value;
-  cat = parseInt(cat);
+  document.getElementById('press').addEventListener('click', myInput)
+function myInput () {
+  dog = Math.floor(Math.random() * 7) + 1
+  cat = document.getElementById('mine').value
+  cat = parseInt(cat)
   if (dog === cat) {
   alert('your answer matched! you got it!')
 } else {
